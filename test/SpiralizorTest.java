404ab9e455787a72baf917d2ead6863f2157f30f
Watson Malcolm <Malcolm.Watson@dematic.com>
2020-03-17 15:46:21 +1100

    Completed SPiralizor
    Completed SPiralizor

---
+++ test/SpiralizorTest.java
@@ -0,0 +1,12 @@
+import code.Spiralizor;
+import org.junit.Test;
+
+public class SpiralizorTest {
+
+    @Test
+    public void test(){
+        int[][] spiral  = Spiralizor.spiralize(5);
+        System.out.println(Spiralizor.spiralize(5));
+
+    }
+}
