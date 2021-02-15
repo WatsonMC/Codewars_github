b745cd97070f0695d559dbd88deeb09dde1594d6
malcolm Watson <watsonmc123@gmail.com>
2021-02-15 19:04:13 +1100

    Adding more tests

--- test/code/BulbGridTest.java
+++ test/code/BulbGridTest.java
@@ -9,11 +9,23 @@ import java.util.List;
 
 public class BulbGridTest {
 
+    static List<BulbGrid.Bulb> FIXED_BULBS = new ArrayList<>();
 
     @Test
     public void firsTest() {
         List<Point> testRes = BulbGrid.switchBulbs(GAME_MAPS[0]);
 
+    }
+
+    public static void createFixedBulbs(){
+        FIXED_BULBS.add(new BulbGrid.Bulb(0,0));
+        FIXED_BULBS.add(new BulbGrid.Bulb(2,0));
+        FIXED_BULBS.add(new BulbGrid.Bulb(4,0));
+        FIXED_BULBS.add(new BulbGrid.Bulb(0,2));
+        FIXED_BULBS.add(new BulbGrid.Bulb(4,2));
+        FIXED_BULBS.add(new BulbGrid.Bulb(0,4));
+        FIXED_BULBS.add(new BulbGrid.Bulb(2,4));
+        FIXED_BULBS.add(new BulbGrid.Bulb(4,4));
     }
 
     @Test
@@ -27,19 +39,10 @@ public class BulbGridTest {
          *   \/    x.x.x
          */
         //T1 All 8 positions successfully found
+        createFixedBulbs();
         List<BulbGrid.Bulb> testBulbs = new ArrayList<>();
-        testBulbs.add(new BulbGrid.Bulb(0,0));
-        testBulbs.add(new BulbGrid.Bulb(2,0));
-        testBulbs.add(new BulbGrid.Bulb(4,0));
-
-        testBulbs.add(new BulbGrid.Bulb(0,2));
-        testBulbs.add(new BulbGrid.Bulb(4,2));
-
-        testBulbs.add(new BulbGrid.Bulb(0,4));
-        testBulbs.add(new BulbGrid.Bulb(2,4));
-        BulbGrid.Bulb bulb4x4 = new BulbGrid.Bulb(4,4);
-        testBulbs.add(bulb4x4);
-
+        testBulbs.addAll(FIXED_BULBS)
+;
         BulbGrid.Bulb middleBulb = new BulbGrid.Bulb(2,2);
 
         Assert.assertEquals(testBulbs,middleBulb.viableBulbs(testBulbs));
@@ -47,13 +50,65 @@ public class BulbGridTest {
         //T2 replaces diagonals
         List<BulbGrid.Bulb> testBulbs2 = new ArrayList<>();
         testBulbs2.addAll(testBulbs);
+
         testBulbs2.remove(new BulbGrid.Bulb(4,4));
+        testBulbs2.remove(new BulbGrid.Bulb(4,4));
+        testBulbs2.remove(new BulbGrid.Bulb(0,4));
+        testBulbs2.remove(new BulbGrid.Bulb(4,0));
+
         testBulbs2.add(new BulbGrid.Bulb(3,3));
         testBulbs.add(new BulbGrid.Bulb(3,3));
 
+        testBulbs2.add(new BulbGrid.Bulb(1,3));
+        testBulbs.add(new BulbGrid.Bulb(1,3));
+
+        testBulbs2.add(new BulbGrid.Bulb(1,1));
+        testBulbs.add(new BulbGrid.Bulb(1,1));
+
+        testBulbs2.add(new BulbGrid.Bulb(3,1));
+        testBulbs.add(new BulbGrid.Bulb(3,1));
+        Assert.assertEquals(testBulbs2,middleBulb.viableBulbs(testBulbs));
+
+        //test left right up down
+        testBulbs.clear();
+        testBulbs.addAll(FIXED_BULBS);
+
+        testBulbs2.clear();
+        testBulbs2.addAll(FIXED_BULBS);
+
+        testBulbs2.remove(new BulbGrid.Bulb(2,4));
+        testBulbs2.remove(new BulbGrid.Bulb(4,2));
+        testBulbs2.remove(new BulbGrid.Bulb(0,2));
+        testBulbs2.remove(new BulbGrid.Bulb(2,0));
+
+        testBulbs.add(new BulbGrid.Bulb(2,3));
+        testBulbs.add(new BulbGrid.Bulb(3,2));
+        testBulbs.add(new BulbGrid.Bulb(1,2));
+        testBulbs.add(new BulbGrid.Bulb(2,1));
+
+        testBulbs2.add(new BulbGrid.Bulb(2,3));
+        testBulbs2.add(new BulbGrid.Bulb(3,2));
+        testBulbs2.add(new BulbGrid.Bulb(1,2));
+        testBulbs2.add(new BulbGrid.Bulb(2,1));
         Assert.assertEquals(testBulbs2,middleBulb.viableBulbs(testBulbs));
 
 
+        //T3 Extra Bulbs
+        testBulbs.clear();
+        testBulbs.addAll(FIXED_BULBS);
+
+        testBulbs2.clear();
+        testBulbs2.addAll(FIXED_BULBS);
+
+        testBulbs.add(new BulbGrid.Bulb(2,5));
+        testBulbs.add(new BulbGrid.Bulb(5,5));
+        testBulbs.add(new BulbGrid.Bulb(25,2));
+
+        Assert.assertEquals(testBulbs2,middleBulb.viableBulbs(testBulbs));
+
+        //Test is empty
+        testBulbs.clear();
+        Assert.assertEquals(testBulbs2,middleBulb.viableBulbs(testBulbs));
 
     }
 
