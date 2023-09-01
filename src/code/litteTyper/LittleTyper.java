8a9f14acb8434b068e0468f4009048e25bf6d8c5
watsonm <watsonmc123@gmail.com>
2023-09-01 23:07:51 +1000

    InsertRegex abstracted

--- src/code/litteTyper/LittleTyper.java
+++ src/code/litteTyper/LittleTyper.java
@@ -33,31 +33,44 @@ public class LittleTyper {
      */
     public static String cleanSpaces(String input){
         String multiSpaceRegex = "[ ]{2,}";
-        int offset = 0;
         String leftBracketEdge =  "(?=(\\(([a-zA-Z0-9]|\\)|\\())).";
         String rightBracketEdge = "(?=(([a-zA-Z0-9]|\\))\\))).";
         String bracketMerge = "\\)\\(";
-        Matcher matcher = Pattern.compile(leftBracketEdge).matcher(input);
-        String tempString = input;
-        while(matcher.find()){
-            tempString = tempString.substring(0,matcher.start()+offset) + "( " + tempString.substring(matcher.start()+offset+1);
-            offset ++;
-        }
-        offset = 0;
-        input = tempString; // offset is variable by char index if pattern matches against original input. I.e no offset at index 0
-        matcher =Pattern.compile(rightBracketEdge).matcher(input);
-        while (matcher.find()){
-            tempString = tempString.substring(0,matcher.start()+offset+1) + " )" + tempString.substring(matcher.start()+offset+2);
-            offset ++;
-        }
-        offset = 0;
-        input = tempString; // offset is variable by char index if pattern matches against original input. I.e no offset at index 0
-        matcher =Pattern.compile(bracketMerge).matcher(input);
-        while (matcher.find()){
-            tempString = tempString.substring(0,matcher.start()+offset) + ") (" + tempString.substring(matcher.start()+offset+2);
-            offset ++;
-        }
+        String arrowLeft = "(?=([a-zA-Z0-9]\\-)).";
+        String arrowRight = "(?=(\\>[a-zA-Z0-9])).";
+        String scLeft = "(?=([a-zA-Z0-9]\\:)).";
+        String scRight = "(?=(\\:([a-zA-Z0-9]|\\())).";
+        String tempString = insertByRegex(leftBracketEdge,input,"( ",0);
+        tempString = insertByRegex(rightBracketEdge,tempString," )", 1);
+        tempString = insertByRegex(bracketMerge,tempString,") (", 0);
+        tempString = insertByRegex(arrowLeft,tempString, " -",1);
+        tempString = insertByRegex(arrowRight,tempString, "> ",0);
+        tempString = insertByRegex(scLeft,tempString, " :",1);
+        tempString = insertByRegex(scRight,tempString, ": ",0);
         return tempString.replaceAll(multiSpaceRegex, " ");
+    }
+
+    /**
+     * Replaces a character based on regex match positions with a passed string
+     * @param regex -> the regex to be matched
+     * @param input -> the input string to be matched against
+     * @param insertText -> the text to replace the character at matcher find location and insert offset
+     * @param insertOffset -> offset from match index that char to replace is at (e.g. if macthing [a-z]\\( to replace  \\( offset = 1
+     * @return String with insertion replacements completed
+     */
+    public static String insertByRegex(String regex, String input, String insertText, int insertOffset){
+        //Start Match Index = matcher.start +insertOffset +offset
+        String tempString = input;
+        Matcher matcher = Pattern.compile(regex).matcher(input);
+        int offset=0;
+        int offsetIncrement = insertText.length()-1;
+        while(matcher.find()){
+            tempString = tempString.substring(0,matcher.start()+offset + insertOffset)
+                    + insertText
+                    + tempString.substring(matcher.start()+offset+offsetIncrement + insertOffset);
+            offset += offsetIncrement;
+        }
+        return tempString;
     }
 
 
--- test/LitteTyperTest.java
+++ test/LitteTyperTest.java
@@ -19,12 +19,37 @@ public class LitteTyperTest {
         Assert.assertEquals(" ", LittleTyper.cleanSpaces(testOnlySpace));
 
         //testing brackets
-
         String bt1 = "(s1)";
-//        Assert.assertEquals("( s1 )",LittleTyper.cleanSpaces(bt1) );
+        Assert.assertEquals("( s1 )",LittleTyper.cleanSpaces(bt1) );
 
         String bt2 = ("( asdf 123) ((asdf))(123 )");
         Assert.assertEquals("( asdf 123 ) ( ( asdf ) ) ( 123 )",LittleTyper.cleanSpaces(bt2) );
+
+        //testing arrows
+        String at1 = "( asdf->123)";
+        Assert.assertEquals("( asdf -> 123 )",LittleTyper.cleanSpaces(at1));
+
+        //testing semi colons
+        String sc1 = "concat:(List -> List -> List)";
+        Assert.assertEquals("concat : ( List -> List -> List )",LittleTyper.cleanSpaces(sc1));
+
+        String contextHard = "id : (A -> B) -> (B -> A) -> (A -> A)\n" +
+                "    inv : (A -> B) -> (B -> A)\n" +
+                "    a:A\n" +
+                "    b:B\n" +
+                "    c:A\n" +
+                "    d:B\n" +
+                "    aB : A -> B\n" +
+                "    bA : B -> A";
+        String contextHardResult = "id : ( A -> B ) -> ( B -> A ) -> ( A -> A )\n" +
+                " inv : ( A -> B ) -> ( B -> A )\n" +
+                " a : A\n" +
+                " b : B\n" +
+                " c : A\n" +
+                " d : B\n" +
+                " aB : A -> B\n" +
+                " bA : B -> A";
+        Assert.assertEquals(contextHardResult,LittleTyper.cleanSpaces(contextHard));
 
     }
 }
