ee9eb946d8f5ef5263d8e8aaa60e4a964ad870e3
watsonmc <watsonmc123@gmail.com>
2023-09-21 18:28:11 +1000

    completed simple interpreter

--- src/code/SimpleInterpreter.java
+++ src/code/SimpleInterpreter.java
@@ -7,31 +7,33 @@ public class SimpleInterpreter {
 public static String movRegex = "mov";
 public static String incRegex = "inc";
 public static String decRegex = "dec";
-public static String jumpRegex = "dec";
+public static String jumpRegex = "jnz";
 public static String regRegex = "[a-zA-Z]+";
 public static String valueRegex = "\\-{0,1}[0-9]+";
 
+
+    public static Map<String, Integer> interpret(String[] program){
         Map<String,Integer> registers = new HashMap<>();
 
         for(int pointer= 0; pointer<program.length;pointer++){
             String[] instructions = program[pointer].split(" ");
             if(instructions[0].matches(movRegex)){
-                Integer newRegValue = instructions[2].matches(valueRegex) ? Integer.valueOf(instructions[2]) : registers.get(instructions[1]);
+                Integer newRegValue = instructions[2].matches(valueRegex) ? Integer.valueOf(instructions[2]) : registers.get(instructions[2]);
                 registers.put(instructions[1],newRegValue);
             }
-            if
-            //inc
-
-            //dec
-            //mov
-                //mov value
-                //mov reg
-            //jump
-                //x !=0
-                //x=0
+            if(instructions[0].matches(incRegex)){
+                registers.put(instructions[1], registers.get(instructions[1])+1);
+            }
+            if(instructions[0].matches(decRegex)){
+                registers.put(instructions[1], registers.get(instructions[1])-1);
+            }
+            if(instructions[0].matches(jumpRegex)){
+                Integer xValue = instructions[1].matches(valueRegex) ? Integer.valueOf(instructions[1]) :registers.get(instructions[1]);
+                Integer yValue = instructions[2].matches(valueRegex) ? Integer.valueOf(instructions[2]) :registers.get(instructions[2]);
+                if(xValue !=0){ pointer += yValue -1;}
+            }
         }
-
-        return null;
+        return registers;
     }
 
 
